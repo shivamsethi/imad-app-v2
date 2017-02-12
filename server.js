5b06be6e70c6886c58018cef128b6417ef4ec415
shivamsethi <shivamsethi2201@gmail.com>
2017-02-13 02:56:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -149,8 +149,8 @@ app.get('/:article_name',function(req,res){
 });
 
 var names=[];
-app.get('/submit-name/:name',function(req,res){
-    var name=req.params.name;
+app.get('/submit-name',function(req,res){
+    var name=req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
 });
