b3ed3b1046383cc13f6b644fd94220c393a5bd0f
shivamsethi <shivamsethi2201@gmail.com>
2017-02-13 01:29:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,2 @@
 console.log('Loaded!');
 
-var element=document.getElementById("main_element");
-element.innerHTML="New value of js code";
-
-var image=document.getElementById("image_id");
-var margin_left=0;
-function moveRight(){
-    margin_left=margin_left+1;
-    image.style.marginLeft=margin_left+'px';
-}
-
-image.onclick=function () {
-    var interval=setInterval(moveRight,5);
-};
