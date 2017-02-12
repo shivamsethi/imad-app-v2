7e5c0ef37cfeee27a84a16ebbb3e394acc4d86d7
shivamsethi <shivamsethi2201@gmail.com>
2017-02-13 01:59:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,8 @@
-console.log('Loaded!');
-
+//counter button
+var counter_button=document.getelementById("counter");
+var count_text=document.getElementById("count");
+var count=0;
+counter_button.onclick=function(){
+    count=count+1;
+    count_text.innerHTML=count.toString();
+};
