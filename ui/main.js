dc089fd6f62b1753eb007f237d989e6f82b0ab58
shivamsethi <shivamsethi2201@gmail.com>
2017-02-13 00:30:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 
 var element=document.getElementById("main_element");
 element.innerHTML="New value of js code";
+
+var image=document.getElementById("image_id");
+image.onclick=function () {
+    image.style.marginLeft='100px';
+};
