a950d3b45d0f91f06347de8e1b98c3c197f36181
shivamsethi <shivamsethi2201@gmail.com>
2017-02-13 02:43:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,7 +29,7 @@ submit_btn.onclick=function(){
     for(var i=0;i<names.length;i++){
         list+='<li>'+names[i]+'</li>';
     }
-    var ul=document.getElementById("nmame_list");
+    var ul=document.getElementById("name_list");
     ul.innerHTML=ul;
     
 };
