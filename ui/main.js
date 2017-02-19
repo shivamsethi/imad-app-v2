71a20715497ee419ca5338c68b6551df2f10af3b
shivamsethi <shivamsethi2201@gmail.com>
2017-02-19 19:09:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,31 +1,31 @@
 alert("start");
 
-var comment_btn=document.getElementById("comment_btn");
-var comment_input=document.getElementById("comment_text");
-comment_btn.onclick=function(){
-    alert("yaa");
-    var request=new XMLHttpRequest();
+// var comment_btn=document.getElementById("comment_btn");
+// var comment_input=document.getElementById("comment_text");
+// comment_btn.onclick=function(){
+//     alert("yaa");
+//     var request=new XMLHttpRequest();
     
-    request.onreadystatechange=function(){
-        if(request.readyState===XMLHttpRequest.DONE)
-        {
-            if(request.status===200)
-            {
-                var comments=JSON.parse(request.responseText);
-                var comment_list='';
-                for(var i=0;i<comments.length;i++)
-                {
-                    comment_list+="<li>"+comments[i]+"</li>";
-                }
-                var ul=document.getElementById("comment_list");
-                ul.innerHTML=comment_list;
-                comment_input.value="";
-            }
-        }
-    };
-    request.open('GET','http://shivamsethi.imad.hasura-app.io/submit_comment/'+comment_input.value,true);
-    request.send(null);
-};
+//     request.onreadystatechange=function(){
+//         if(request.readyState===XMLHttpRequest.DONE)
+//         {
+//             if(request.status===200)
+//             {
+//                 var comments=JSON.parse(request.responseText);
+//                 var comment_list='';
+//                 for(var i=0;i<comments.length;i++)
+//                 {
+//                     comment_list+="<li>"+comments[i]+"</li>";
+//                 }
+//                 var ul=document.getElementById("comment_list");
+//                 ul.innerHTML=comment_list;
+//                 comment_input.value="";
+//             }
+//         }
+//     };
+//     request.open('GET','http://shivamsethi.imad.hasura-app.io/submit_comment/'+comment_input.value,true);
+//     request.send(null);
+// };
 
 
 alert("eeeee");
